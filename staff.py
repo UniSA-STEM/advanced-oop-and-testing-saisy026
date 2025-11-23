d5e5647f7dd39cc5298ac8daf883bf732703d53d
saisy026 <saisy026@mymail.unisa.edu.au>
2025-11-23 18:53:40 +1030

    assign methods added to staff.

--- staff.py
+++ staff.py
@@ -6,6 +6,10 @@ ID: 110434667
 Username: saisy026
 This is my own work as defined by the University's Academic Integrity Policy.
 """
+from animal import Animal
+from enclosure import Enclosure
+from health_record import HealthRecord
+
 class Staff:
     def __init__(self, name, employee_id, role):
         if not name or not isinstance(name, str):
@@ -18,9 +22,34 @@ class Staff:
         self._name = name
         self._employee_id = employee_id
         self._role = role
+        self._assigned_animals = []  # Animals staff member looks after
+        self._assigned_enclosures = []  # Enclosures staff member is responsible for
+
+# Getters
 
     def get_name(self):
         return self._name
 
     def get_role(self):
         return self._role
+
+    def get_assigned_animals(self):
+        return self._assigned_animals
+
+    def get_assigned_enclosures(self):
+        return self._assigned_enclosures
+
+# Assign methods
+
+    def assign_to_animal(self, animal):
+        if not isinstance(animal, Animal):
+            raise ValueError("Can only assign Animal")
+        if animal not in self._assigned_animals:
+            self._assigned_animals.append(animal)
+
+    def assign_to_enclosure(self, enclosure):
+        if not isinstance(enclosure, Enclosure):
+            raise ValueError("Can only assign Enclosure")
+        if enclosure not in self._assigned_enclosures:
+            self._assigned_enclosures.append(enclosure)
+
