5183228576e8b2c8d9dcb4a14131e4f4fb43a3c5
saisy026 <saisy026@mymail.unisa.edu.au>
2025-11-24 01:54:39 +1030

    methods added to zoo

--- zoo.py
+++ zoo.py
@@ -6,6 +6,8 @@ ID: 110434667
 Username: saisy026
 This is my own work as defined by the University's Academic Integrity Policy.
 """
+from animal import Animal
+from datetime import datetime
 
 class Zoo:
     def __init__(self):
@@ -25,4 +27,27 @@ class Zoo:
             return False
         self._enclosures.append(enclosure)
         return True
+
+    def add_staff(self, staff):
+        self._staff.append(staff)
+
+    def generate_animal_report(self):
+        print(f"\n Animal Report for {self._name}")
+        for animal in self._animals:
+            enc = animal.get_enclosure().get_enclosure_id() if animal.get_enclosure() else "None"
+            status = animal.get_health_status()
+            print(f"Name: {animal.get_name()}\n"
+                  f"Species: {animal.get_species()}\n"
+                  f"Age: {animal.get_age()}\n"
+                  f"Enclosure: {enc}\n"
+                  f"Health: {status}")
+
+    def generate_enclosure_report(self):
+        print(f"\n Enclosure Status Report")
+        for enc in self._enclosures:
+            print(enc)
+
+    def get_animals_needing_attention(self):
+        return [a for a in self._animals if a.is_under_treatment()]
+
 
