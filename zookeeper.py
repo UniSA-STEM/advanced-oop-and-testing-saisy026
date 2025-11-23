a9daff1454f6d3bdcea44e3107fb257e2feb7264
saisy026 <saisy026@mymail.unisa.edu.au>
2025-11-23 23:04:58 +1030

    Methods added to zookeeper.

--- zookeeper.py
+++ zookeeper.py
@@ -7,7 +7,15 @@ Username: saisy026
 This is my own work as defined by the University's Academic Integrity Policy.
 """
 from staff import Staff
+from enclosure import Enclosure
 
 class Zookeeper(Staff):
     def __init__(self, name, employee_id):
         super().__init__(name, employee_id,"zookeeper")
+
+    def clean_enclosure(self, enclosure):
+        if not isinstance(enclosure, Enclosure):
+            raise ValueError("Invalid enclosure")
+        if enclosure not in self._assigned_enclosures:
+            return f"{self._name} is not assigned to clean enclosure {enclosure.get_enclosure_id()}"
+        return f"Zookeeper {self._name} {enclosure.clean(50)}"
